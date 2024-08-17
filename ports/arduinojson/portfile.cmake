93961788278f30501b1f58b95966e61a18128ebf
Florian Weßel <florianwessel@gmx.net>
2024-08-17 12:57:59 +0200

    Update arduinojson version port file

--- ports/arduinojson/portfile.cmake
+++ ports/arduinojson/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO bblanchon/ArduinoJson
-    REF ${VERSION}
+    REF v${VERSION}
     SHA512 df2a89c8f9a03ebb6c2ebd014092e6802826e271a06a76d267ad21cd062f6f60adb058f47f352a6155c3f5407be609815cbcda29cb98f18cfb5283cf6563cbbd
     HEAD_REF 6.x
 )
