897b44a6c182795b1492c1e521fd7a6a1a7986cc
Florian Weßel <florianwessel@gmx.net>
2024-11-21 21:58:10 +0100

    Update trompeloeil sha512

--- ports/trompeloeil/portfile.cmake
+++ ports/trompeloeil/portfile.cmake
@@ -2,7 +2,7 @@ vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO rollbear/trompeloeil
     REF v${VERSION}
-    SHA512 0
+    SHA512 f1a7212eacfb79f73cea075a147066b4cb10da76a6826e4594c29412395ae69647bb852d12936dd3f9b5c5a7f0aad3ebae246fdfb4006072c39b1efcd51876f5
     HEAD_REF master
 )
 
